d0e4b7b34bccea9ddd8b85ebfafa811459749484
CarlosSanchezVicente <sanchezvicentecarlos@gmail.com>
2024-10-03 08:45:25 +0200

    DB_problem

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # IMPORTS LIBRARIES
 import streamlit as st
 import pandas as pd
-from dotenv import dotenv_values 
+#from dotenv import dotenv_values 
+import streamlit_authenticator as stauth
+import hmac
 
 # IMPORT FUNCTIONS FROM MODULES
 from modules import notion_read_transform as notion
@@ -66,6 +68,42 @@ ID_dict_elec = {'MethaneLine':{'db_name': 'data_methane_line',
           }
 
 
+# AUTHENTICATION
+def check_password():
+    """Returns `True` if the user had a correct password."""
+
+    def login_form():
+        """Form with widgets to collect user information"""
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["username"] in st.secrets[
+            "passwords"
+        ] and hmac.compare_digest(
+            st.session_state["password"],
+            st.secrets.passwords[st.session_state["username"]],
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the username or password.
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show inputs for username + password.
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("😕 User not known or password incorrect")
+    return False
+
+
 # MAIN FUNCTION
 def main():
     # STREAMLIT CODE
@@ -108,5 +146,8 @@ def main():
 
 # MAIN EXECUTION
 if __name__ == '__main__':
+    if not check_password():
+        st.stop()
+
     result = main()
     #print(result)
