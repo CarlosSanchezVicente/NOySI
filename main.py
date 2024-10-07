e9261714b9409bdfd097fa928e73a3f48a247473
CarlosSanchezVicente <sanchezvicentecarlos@gmail.com>
2024-10-07 14:01:41 +0200

    authentication

--- main.py
+++ main.py
@@ -135,7 +135,7 @@ def main():
     # STREAMLIT CODE
     # Add research name group and logos
     add_logo()
-    st.logo('./img/noysi.png', size="small")
+    st.logo('./img/NoySI.png', size="small")
 
     # Inicializate 'authentication_status' variable
     if 'authentication_status' not in st.session_state:
